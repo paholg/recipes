1b239ce0ee068755ab6f4762b78ad72732898d76
Paho Lurie-Gregg <paho@paholg.com>
2014-10-12 22:24:15 -0700

    Changed method of formatting recipe files. They are no longer .tex files, now they are simpler and turned into tex with mkbook.py

--- mkbook.py
+++ mkbook.py
@@ -8,15 +8,9 @@ begin = r"""
 \usepackage{array}
 \usepackage[margin=0.5in]{geometry}
 
-\renewcommand{\l}{\newline}
 \def\arraystretch{2}
 
 \begin{document}
-
-\title{Paho's Collection of Recipes}
-\date{\today}
-
-\author{Paho Lurie-Gregg}
 
 \newcounter{rowcount}
 
@@ -28,7 +22,42 @@ Run with no arguments to make a full recipe book.
 Run with "toc" as an argument to add a table of contents.
 Run with a list of .tex files to use just those recipes.
 See the included recipes for a formatting example to add more recipes.
+Format:
+  The first line should be the recipe name.
+  Then, list steps as follows:
+    List the instructions for a step, then an '&' to begin listing ingredients.
+    When finished with a step, use a '^', with each separating character on its own line.
+Recipe files should end in .rec
 """)
+
+# takes recipe files and turns them into latex
+def translate(filename):
+    f = open(filename, 'r')
+    recipe = [l.strip() for l in f.readlines() if l.strip()]
+    name = recipe[0]
+    head = '\\section{%s}\n'  %name + \
+    '\\setcounter{rowcount}{0}\n' + \
+    r'\begin{tabular}{@{\stepcounter{rowcount}\therowcount)\hspace*{\tabcolsep}}p{.64\textwidth}p{.3\textwidth}}' + '\n'
+
+    body = ""
+    mid_step = False
+    for line in recipe[1:]:
+        if '^' in line or '&' in line:
+            mid_step = False
+        else:
+            if mid_step:
+                body += '\\newline\n'
+            mid_step = True
+
+        if '^' in line:
+            line = line.replace('^', r'\\\hline')
+        body += line + '\n'
+
+
+    foot = "\\end{tabular}\n\n"
+    return head + body + foot
+
+
 
 if len(sys.argv) == 1 or (len(sys.argv) == 2 and 'toc' in sys.argv):
     files = sorted(glob.glob("*.tex"))
@@ -50,7 +79,7 @@ middle = ""
 
 for f in files:
     if os.path.isfile(f):
-        middle += '\\input{%s}\n' %f
+        middle += translate(f)
     else:
       print('Error: %s does not exist.' %f)
       exit(17)
